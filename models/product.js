ca82cb3dcdcca2475c76de21aadce08320d36e20
ShivamSaini26 <shivam.saini2126@gmail.com>
2023-07-18 09:04:07 +0530

    minor changes

--- models/product.js
+++ models/product.js
@@ -1,64 +1,27 @@
-/* without sequelize model
+const Sequelize = require('sequelize');
 
+const sequelize = require('../util/database');
 
-const db = require('../util/database');
-
-const Cart = require('./cart');
-
-module.exports = class Product {
-  constructor(id, title, imageUrl, description, price) {
-    this.id = id;
-    this.title = title;
-    this.imageUrl = imageUrl;
-    this.description = description;
-    this.price = price;
-  }
-
-  save() {
-    return db.execute(
-      'INSERT INTO products (title, price, imageUrl, description) VALUES (?, ?, ?, ?)',
-      [this.title, this.price, this.imageUrl, this.description]
-    );
-  }
-
-  static deleteById(id) {}
-
-  static fetchAll() {
-    return db.execute('SELECT * FROM products');
-  }
-
-  static findById(id) {
-    return db.execute('SELECT * FROM products WHERE products.id = ?', [id]);
-  }
-};
-
-*/
-
-//after sequelize model
-const Sequelize = require("sequelize");
-
-const sequelize = require("../util/database");
-
-const Product = sequelize.define("product", {
+const Product = sequelize.define('product', {
   id: {
     type: Sequelize.INTEGER,
     autoIncrement: true,
     allowNull: false,
-    primaryKey: true,
+    primaryKey: true
   },
   title: Sequelize.STRING,
   price: {
     type: Sequelize.DOUBLE,
-    allowNull: false,
+    allowNull: false
   },
   imageUrl: {
     type: Sequelize.STRING,
-    allowNull: false,
+    allowNull: false
   },
   description: {
     type: Sequelize.STRING,
-    allowNull: false,
-  },
+    allowNull: false
+  }
 });
 
 module.exports = Product;
--- util/database.js
+++ util/database.js
@@ -1,25 +1,8 @@
-/*
-connectivity without sequelize package
+const Sequelize = require('sequelize');
 
-const mysql = require('mysql2');
-
-const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'node-complete',
-    password: 'shivam26'
+const sequelize = new Sequelize('node-complete', 'root', 'shivam26', {
+  dialect: 'mysql',
+  host: 'localhost'
 });
 
-module.exports = pool.promise();
-*/
-
-//connectivity with sequelize package
-
-const Sequelize = require("sequelize");
-
-const sequelize = new Sequelize("node-complete", "root", "shivam26", {
-  dialect: "mysql",
-  host: "localhost",
-});
-
-module.exports=sequelize;
+module.exports = sequelize;
