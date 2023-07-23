2ce226a535e0b0e614aad04fbe4be8d399f6ef29
ShivamSaini26 <shivam.saini2126@gmail.com>
2023-07-23 20:54:28 +0530

    adding cart and cart-item model

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const sequelize = require('./util/database');
 
 const Product = require('./models/product');
 const User = require('./models/user');
+const Cart = require('./models/cart');
+const CartItem = require('./models/cart-items');
 
 
 const app = express();
@@ -37,12 +39,17 @@ app.use(errorController.get404);
 Product.belongsTo(User, {
   constraints: true,
   onDelete: 'CASCADE'
-});
+});//associations
 User.hasMany(Product);
+User.hasOne(Cart);
+Cart.belongsTo(User);
+Cart.belongsToMany(Product,{through:CartItem});
+Product.belongsToMany(Cart, { through: CartItem });
 
 //static user account
 sequelize
-  .sync().then(result => {
+  // .sync({ force: true })  
+.sync().then(result => {
     return User.findByPk(1);
   }).then(user => {
     if (!user) {
@@ -52,8 +59,8 @@ sequelize
   })
   .then(user => {
     // console.log(user);
-    app.listen(3000);
-  })
+    return user.createCart();
+  }).then(cart => app.listen(3000))
   .catch(err => {
     console.log(err);
   });
