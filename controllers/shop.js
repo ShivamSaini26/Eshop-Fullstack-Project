5fdd3eb792cc67d2e19e33f9a5bf21e8622975ef
ShivamSaini26 <shivam.saini2126@gmail.com>
2023-07-24 21:00:29 +0530

    CREATING AND FETCHING CART, ADDING NEW PRODUCTS, RETREIVE PRODUCTS, DELETE PRODUCTS (CRUD) OPERATION - 167-170

--- controllers/shop.js
+++ controllers/shop.js
@@ -56,8 +56,10 @@ exports.getIndex = (req, res, next) => {
 exports.getCart = (req, res, next) => {
   req.user
     .getCart()
-    .then(cart=>{console.log(cart)
-    return cart.getProducts()}).then(products=>{
+    .then(cart => {
+      console.log(cart)
+      return cart.getProducts()
+    }).then(products => {
       res.render('shop/cart', {
         path: '/cart',
         pageTitle: 'Your Cart',
@@ -68,19 +70,44 @@ exports.getCart = (req, res, next) => {
 };
 
 exports.postCart = (req, res, next) => {
+
   const prodId = req.body.productId;
-  Product.findByPk(prodId, (product) => {
-    Cart.addProduct(prodId, product.price);
-  });
-  res.redirect("/cart");
+  let fetchedCart;
+  newQuantity=1;
+  req.user.getCart().then(cart=>{
+    fetchedCart=cart;
+    return cart.getProducts({where:{ id:prodId}});
+  }).then(products=>{
+    let product;
+    if(products.length>0){
+      product=products[0];
+    }
+if(product){const oldQuantity=product.cartItem.quantity;
+  newQuantity=oldQuantity+1;
+  return product;
+}
+return Product.findByPk(prodId);
+  }).then(product=>{
+    return fetchedCart.addProduct(product,{
+      through:{quantity:newQuantity}
+    });
+  }).then(()=>{
+res.redirect('/cart');
+  }).catch(err=>console.log(err));
 };
 
 exports.postCartDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.findByPk(prodId, (product) => {
-    Cart.deleteProduct(prodId, product.price);
-    res.redirect("/cart");
-  });
+  req.user.getCart().then(cart=>{
+    return cart.getProducts({where:{
+      id:prodId
+    }}).then(products=>{
+const product=products[0];
+return product.cartItem.destroy();
+    }).then(result=>{
+      res.redirect('/cart');
+    })
+  }).catch(err=>console.log(err));
 };
 
 exports.getOrders = (req, res, next) => {
